b09f0db0d226cee426535451ece3e0a0dbe6f133
LAlbertoA <luis.arcos@ciencias.unam.mx>
2026-03-22 16:00:31 -0600

    NOW removed build/ from repo

--- build/CMakeFiles/env_monitor.dir/src/debug_state.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/env_monitor.dir/src/debug_state.c.o: \
- /home/luis-arcos/Documents/RaspberryPiPico/PicoW_GHT_Oled/src/debug_state.c \
- /home/luis-arcos/Documents/RaspberryPiPico/PicoW_GHT_Oled/src/debug_state.h \
- /usr/lib/gcc/arm-none-eabi/13.2.1/include/stdint.h
